a5321307a3319e1dd2acad54e996f00d39449b1c
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-09 19:29:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,56 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+function myShoppingList() {
+	let button = document.getElementById("enter");
+	let input = document.getElementById("userinput");
+	let ul = document.querySelector("ul");
 
-function inputLength() {
-	return input.value.length;
+	function inputLength() {
+		return input.value.length;
+	}
+
+	function createListElement() {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		addDeleteButton(li);
+		ul.appendChild(li);
+		input.value = "";
+	}
+
+	function addListAfterClick() {
+		if (inputLength() > 0) {
+			createListElement();
+		}
+	}
+
+	function addListAfterKeypress(event) {
+		if (inputLength() > 0 && event.keyCode === 13) {
+			createListElement();
+		}
+	}
+
+	button.addEventListener("click", addListAfterClick);
+
+	input.addEventListener("keypress", addListAfterKeypress);
+
+	let li = document.getElementsByTagName("li");
+
+	for (let i = 0; i < li.length; i++) {
+		li[i].addEventListener("click", toggleList);
+		addDeleteButton(li[i]);
 }
+	//function to add the 'line through' style
+	function toggleList() {
+		this.classList.toggle("done");
+	}
 
-function createListElement() {
-	var li = document.createElement("li");
-	// console.log(li);
-	li.appendChild(document.createTextNode(input.value));
-	addDeleteButton(li);
-	ul.appendChild(li);
-	input.value = "";
-}
+	function removeList() {
+		this.parentElement.remove();
+	}
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
+	function addDeleteButton(parent) {
+		let button = parent.appendChild(document.createElement("button"));
+		button.appendChild(document.createTextNode("delete"));
+		button.onclick = removeList;
 	}
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
-/**
- * 
- * 1. If you click on the list item, it toggles the .done  
- * class on and off.
- * 
-
-2. Add buttons next to each list item to delete the item 
-when clicked on its corresponding delete button.
-
-3. BONUS: When adding a new list item, it automatically 
-adds the delete button next to it (hint: be sure to check 
-	if new items are clickable too!)
- * 
- */
-
-var li = document.getElementsByTagName("li");
-
-for (var i = 0; i < li.length; i++) {
-	li[i].addEventListener("click", toggleList);
-	addDeleteButton(li[i]);
-}
-
-//function to add the 'line through' style
-function toggleList() {
-	this.classList.toggle("done");
-}
-
-function removeList() {
-	this.parentElement.remove();
-}
-
-function addDeleteButton(parent) {
-	//creo il bottone
-	var button = parent.appendChild(document.createElement("button"));
-	button.appendChild(document.createTextNode("delete"));
-	
-	button.onclick = removeList;
-	
-}
-
-////////////////////////////////////////////////
-//alternative
-
-// var li = document.querySelectorAll("li");
-
-// for (var i=0; i<li.length; i++) {
-// 	li[i].addEventListener("click", toggleList);
-// }
-
-// function toggleList(event) {
-// 	event.currentTarget.classList.toggle("done");
-// }
+myShoppingList();
