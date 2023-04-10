db9936c948f661521fcbb755c52598eb9eb141cb
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-10 19:06:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,6 @@ function myShoppingList() {
 
 	button.addEventListener("click", addListAfterClick);
 	btnClear.addEventListener("click", clearList);
-
 	input.addEventListener("keypress", addListAfterKeypress);
 
 	let li = document.getElementsByTagName("li");
@@ -60,6 +59,35 @@ function myShoppingList() {
 		button.id = "btn_delete";
 		button.onclick = removeList;
 	}
-}
 
+	function updatedList(listValue) {
+        const list = document.querySelector('#myList');
+        list.textContent = listValue;
+		if (typeof listValue === 'object' && listValue !== null) {
+			localStorage.setItem('myList', JSON.stringify(listValue));
+		} else {
+			console.error('Invalid JSON string');
+		}
+
+    }
+
+	function loadListValue() {
+        const savedListValue = localStorage.getItem('myList');
+		if (savedListValue !== undefined) {
+			const parsedListValue = JSON.parse(savedListValue);
+			document.querySelector('#myList').textContent = parsedListValue;
+			document.querySelector('#saved_list').textContent = 'saved list';
+		} else {
+			console.error('Invalid JSON string');
+		}
+    }
+    
+    const loadButton = document.querySelector('#load_button');
+	loadButton.onclick = loadListValue;
+    loadButton.addEventListener('click', () => {
+		const listValue = document.querySelector('#myList').value;
+		updatedList(listValue);
+	});
+	
+}
 myShoppingList();
