cfe5eb89161ddc702a284f5f114629822ee8b41d
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-16 20:58:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,91 @@
 const myShoppingList = () => {
-	const button = document.getElementById("enter");
-	const input = document.getElementById("userinput");
-	const ul = document.querySelector("ul");
-	const btnClear = document.getElementById("clear");
+    const button = document.getElementById('enter');
+    const input = document.getElementById('userinput');
+    const ul = document.querySelector('ul');
+    const btnClear = document.getElementById('clear');
 
-	const inputLength = () => {
-		return input.value.length;
-	}
+    const inputLength = () => {
+        return input.value.length;
+    };
 
-	const createListElement = () => {
-		let li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		addDeleteButton(li);
-		ul.appendChild(li);
-		input.value = "";
-	}
+    const createListElement = () => {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(input.value));
+        addDeleteButton(li);
+        ul.appendChild(li);
+        input.value = '';
+    };
 
-	const addListAfterClick = () => {
-		if (inputLength() > 0) {
-			createListElement();
-		}
-	}
+    const addListAfterClick = () => {
+        if (inputLength() > 0) {
+            createListElement();
+        }
+    };
 
-	const addListAfterKeypress = (event) => {
-		if (inputLength() > 0 && event.keyCode === 13) {
-			createListElement();
-		}
-	}
+    const addListAfterKeypress = (event) => {
+        if (inputLength() > 0 && event.keyCode === 13) {
+            createListElement();
+        }
+    };
 
-	const clearList = () => {
-		while (ul.firstChild) {
-			ul.removeChild(ul.firstChild);
-		}
-	}
+    const clearList = () => {
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+    };
 
-	button.addEventListener("click", addListAfterClick);
-	btnClear.addEventListener("click", clearList);
-	input.addEventListener("keypress", addListAfterKeypress);
+    button.addEventListener('click', addListAfterClick);
+    btnClear.addEventListener('click', clearList);
+    input.addEventListener('keypress', addListAfterKeypress);
 
-	let li = document.getElementsByTagName("li");
+    let li = document.getElementsByTagName('li');
 
-	for (let i = 0; i < li.length; i++) {
-		li[i].addEventListener("click", toggleList);
-		addDeleteButton(li[i]);
-	}
-	//function to add the 'line through' style
-	function toggleList() {
-		this.classList.toggle("done");
-	}
+    for (let i = 0; i < li.length; i++) {
+        li[i].addEventListener('click', toggleList);
+        addDeleteButton(li[i]);
+    }
+    //function to add the 'line through' style
+    function toggleList() {
+        this.classList.toggle('done');
+    }
 
-	function removeList() {
-		this.parentElement.remove();
-	}
+    function removeList() {
+        this.parentElement.remove();
+    }
 
-	function addDeleteButton(parent) {
-		let button = parent.appendChild(document.createElement("button"));
-		button.appendChild(document.createTextNode("delete"));
-		button.id = "btn_delete";
-		button.onclick = removeList;
-	}
+    function addDeleteButton(parent) {
+        let button = parent.appendChild(document.createElement('button'));
+        button.appendChild(document.createTextNode('delete'));
+        button.id = 'btn_delete';
+        button.onclick = removeList;
+    }
 
-	const updatedList = (listValue) => {
+    const updatedList = (listValue) => {
         const list = document.querySelector('#myList');
         list.textContent = listValue;
-		if (typeof listValue === 'object' && listValue !== null) {
-			localStorage.setItem('myList', JSON.stringify(listValue));
-		} else {
-			console.error('Invalid JSON string');
-		}
+        if (typeof listValue === 'object' && listValue !== null) {
+            localStorage.setItem('myList', JSON.stringify(listValue));
+        } else {
+            console.error('Invalid JSON string');
+        }
+    };
 
-    }
-
-	const loadListValue = () => {
+    const loadListValue = () => {
         const savedListValue = localStorage.getItem('myList');
-		if (savedListValue !== undefined) {
-			const parsedListValue = JSON.parse(savedListValue);
-			document.querySelector('#myList').textContent = parsedListValue;
-			document.querySelector('#saved_list').textContent = 'saved list';
-		} else {
-			console.error('Invalid JSON string');
-		}
-    }
-    
+        if (savedListValue !== undefined) {
+            const parsedListValue = JSON.parse(savedListValue);
+            document.querySelector('#myList').textContent = parsedListValue;
+            document.querySelector('#saved_list').textContent = 'saved list';
+        } else {
+            console.error('Invalid JSON string');
+        }
+    };
+
     const loadButton = document.querySelector('#load_button');
-	loadButton.onclick = loadListValue;
+    loadButton.onclick = loadListValue;
     loadButton.addEventListener('click', () => {
-		const listValue = document.querySelector('#myList').value;
-		updatedList(listValue);
-	});
-	
-}
+        const listValue = document.querySelector('#myList').value;
+        updatedList(listValue);
+    });
+};
 myShoppingList();
